c65f90bac0b204d3d0115d31bc38b96fdbea7407
Davide Quaranta <30431538+fortym2@users.noreply.github.com>
2021-12-08 17:09:31 +0100

    Add parameter parsing to evaluation script

--- benchmarks/all_against_all.py
+++ benchmarks/all_against_all.py
@@ -1,19 +1,38 @@
+import argparse
+import random
 from deepface import DeepFace
 
-INPUT_PATHS = "../utils/file_list_short_short.txt"
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="File containing the dataset's file paths")
+parser.add_argument("-s", "--shuffle", help="Whether to shuffle the paths list", action="store_true")
+parser.add_argument("-l", "--limit", help="Whether to limit the (eventually shuffled) paths list to a certain length", type=int)
+parser.add_argument("-t", "--thresholds", nargs="+", action="extend", help="A list of thresholds to test", type=float)
+parser.add_argument("-dc", "--cosine", help="Test with the cosine distance metric", action="store_true")
+parser.add_argument("-de", "--euclidean", help="Test with the euclidean distance metric", action="store_true")
+parser.add_argument("-del2", "--euclidean_l2", help="Test with the euclidean_l2 distance metric", action="store_true")
+args = parser.parse_args()
 
-THRESHOLDS = [0.2, 0.3]
-
-METRICS = ["cosine", "euclidean", "euclidean_l2"]
+distance_metrics = []
+if args.cosine:
+    distance_metrics.append("cosine")
+if args.euclidean:
+    distance_metrics.append("euclidean")
+if args.euclidean_l2:
+    distance_metrics.append("euclidean_l2")
 
 model = DeepFace.build_model('VGG-Face')
 
 def verify(distance_score, threshold):
     return distance_score <= threshold
 
-with open(INPUT_PATHS) as file:
+with open(args.input) as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
+    if args.shuffle:
+        random.shuffle(lines)
+    if args.limit is not None:
+        lines = lines[:args.limit]
+    total_combinations = (len(lines)**2 - len(lines)) * len(args.thresholds) * len(distance_metrics)
 
 genuine_acceptances = dict()
 genuine_rejections = dict()
@@ -24,9 +43,8 @@ genuine_attempts = dict()
 impostor_attempts = dict()
 
 def perform_all_against_all():
-
-    for metric in METRICS:
-
+    current_combination = 1
+    for metric in distance_metrics:
         genuine_acceptances[metric] = dict()
         genuine_rejections[metric] = dict()
         false_acceptances[metric] = dict()
@@ -35,7 +53,7 @@ def perform_all_against_all():
         genuine_attempts[metric] = dict()
         impostor_attempts[metric] = dict()
 
-        for threshold in THRESHOLDS:
+        for threshold in args.thresholds:
             threshold_str = str(threshold)
 
             genuine_acceptances[metric][threshold_str] = 0
@@ -60,11 +78,16 @@ def perform_all_against_all():
                     else:
                         impostor_attempts[metric][threshold_str] += 1
 
-                    result = DeepFace.verify(
-                        img1_path = lines[first_identity_index],
-                        img2_path = lines[second_identity_index],
-                        model = model
-                    )
+                    try:
+                        result = DeepFace.verify(
+                            img1_path = lines[first_identity_index],
+                            img2_path = lines[second_identity_index],
+                            model = model
+                        )
+                    except ValueError as e:
+                        # happens when the img cannot be loaded
+                        print(e, lines[first_identity_index], lines[second_identity_index])
+                        continue
 
                     verified = verify(result['distance'], threshold)
 
@@ -76,17 +99,18 @@ def perform_all_against_all():
 
                     false_rejections[metric][threshold_str] += int(not verified and (first_identity_name == second_identity_name))
 
-                    print("matching faces: ", first_identity_index, ", ", first_identity_name, " VS. ", second_identity_index, ", ", second_identity_name)
-                    print("DeepFace says: ", result['verified'])
-                    print("Should actually be: ", result['verified'] and (first_identity_name == second_identity_name))
-                    print("Distance: ", result['distance'], ", threshold: ", threshold)
-                    print()
+                    print(f"Matching faces: {first_identity_index} ({first_identity_name}) VS {second_identity_index} ({second_identity_name})")
+                    print(f"DeepFace says:  {result['verified']}")
+                    print(f"Should be:      {result['verified']} and (first_identity_name == second_identity_name)")
+                    print(f"Distance:       {result['distance']}")
+                    print(f"Threshold:      {threshold}")
+                    print(f"Progress:       {current_combination}/{total_combinations} [{round(current_combination/total_combinations*100, 2)}%]")
+                    print("----------------------------------------------------------------------------------------")
+                    current_combination += 1
 
 def print_recognition_metrics():
-    for metric in METRICS:
-
-        for threshold in THRESHOLDS:
-
+    for metric in distance_metrics:
+        for threshold in args.thresholds:
             threshold_str = str(threshold)
 
             # print("GA[", threshold_str, "]: ", genuine_acceptances[metric][threshold_str], genuine_attempts[metric][threshold_str])
