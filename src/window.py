918548222a1813a105d8e7e67ad99ce37e6e20b0
Davide Quaranta <30431538+fortym2@users.noreply.github.com>
2021-12-21 20:59:11 +0100

    Add some style to windows
    Related to #6

--- src/window.py
+++ src/window.py
@@ -1,22 +1,34 @@
-from utils import OPERATIONS_WINDOW_LABELS, OPERATIONS_WINDOW_TITLES
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
+from utils import OPERATIONS_WINDOW_LABELS, OPERATIONS_WINDOW_TITLES
 
 
 class Window:
     def __init__(self, operation: int):
         self.window = tk.Tk()
         self.window.title(OPERATIONS_WINDOW_TITLES[operation])
-        self.label = tk.Label(self.window, text=OPERATIONS_WINDOW_LABELS[operation])
+
+        self.label = tk.Label(
+            self.window,
+            text=OPERATIONS_WINDOW_LABELS[operation],
+            pady=20,
+            font=("sans-serif", 24),
+        )
         self.canvas = tk.Label(self.window)
         self.button = tk.Button(
-            self.window, text="Shot picture", command=self._destroy_with_success
+            self.window,
+            text="Shot picture",
+            padx=20,
+            pady=10,
+            font=("sans-serif", 18, "bold"),
+            command=self._destroy_with_success,
         )
 
         self.label.pack()
         self.canvas.pack()
-        self.button.pack()
+        self.button.pack(pady=20)
+        self.button.focus_set()
 
         self.shot_button_pressed = False
         self.capture = cv2.VideoCapture(0)
