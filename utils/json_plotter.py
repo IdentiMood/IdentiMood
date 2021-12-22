f984d22d1c51e02ffc475e0d26c4a4ea92c85732
Daniele Solombrino <daniele.solombrino@gmail.com>
2021-12-22 16:39:19 +0100

    [COMPLETE] read json from file, initial restructure of plot methods to take params from outside

--- utils/json_plotter.py
+++ utils/json_plotter.py
@@ -1,13 +1,41 @@
-from os import TMP_MAX
-import plotter
+from plotter import plot
 import json
 import sys
+import time
+from datetime import datetime
+import os
+import argparse
 
-json_path = sys.argv[1]
-print(json_path)
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-p", "--show-plot", 
+    help = "Whether to show the plots as they are computed", action="store_true"
+)
+parser.add_argument(
+    "-d", "--plot-save-dir", help = "Directory in which to store computed plots"
+)
+parser.add_argument(
+    "-i", "--input-json", help = "JSON storing data to compute plots from"
+)
+
+args = parser.parse_args()
+
+if args.input_json == None:
+    print("ERROR: Must specify a JSON file path.")
+    exit(-1)
+
+if args.plot_save_dir == None:
+    args.plot_save_dir = "./plots"
+
+for sub_dir in ["/threshold_vs_frr_far", "/roc", "/det"]:
+    if not os.path.exists(args.plot_save_dir + sub_dir):
+        os.makedirs(args.plot_save_dir + sub_dir)
+
+print(args)
 
 # Opening JSON file
-f = open(json_path)
+f = open(args.input_json)
 
 # returns JSON object as a dictionary
 json_content = json.load(f)
@@ -16,17 +44,72 @@ json_content = json.load(f)
 f.close()
 
 metrics = list(json_content["genuine_acceptances"])
-print(metrics)
 
 models_temp_key = list(json_content["genuine_acceptances"])[0]
 models = list(json_content["genuine_acceptances"][models_temp_key])
-print(models)
 
 thresholds = list(json_content["genuine_acceptances"][metrics[0]][models[0]])
-print(thresholds)
 
+time_stamp = datetime.fromtimestamp(time.time()).strftime('%y_%m_%d_%H-%M-%S')
 
-# threshold_FAR_FRR
-# for metric in metrics:
+# plotting:
+# threshold vs. FAR & FRR --> x = thresholds, y = FRR, FAR
+# ROC                     --> x = FAR, y = GAR
+# DET (logarithmic scale) --> x = FAR, y = FRR
+for metric in metrics:
+    for model in models: 
+        far = list(json_content["false_acceptance_rate"][metric][model].values())
 
-#   for model in models:
+        frr = list(json_content["false_rejection_rate"][metric][model].values())
+
+        plot_file_full_path = args.plot_save_dir + "/threshold_vs_frr_far/" + \
+            metric + "_" + model + "_" + time_stamp + ".png"
+
+        # plotting:
+        # threshold vs. FAR & FRR --> x = thresholds, y = FRR, FAR
+        plot(
+            x_axis = [far, frr],
+            y_axis = [thresholds, thresholds],
+            x_label = "thresholds", y_label = "FRR, FAR",
+            line_label = [ "False Rejection Rate", "False Acceptance Rate" ],
+            plot_name = f"thresholds VS. FRR & FAR\nDistance metric: {metric}. Deep Learning model: {model}",
+            show_plot = args.show_plot,
+            plot_file_full_path = plot_file_full_path,
+            x_axis_scale = "linear", y_axis_scale = "linear"
+        )
+
+        gar = list(json_content["genuine_acceptance_rate"][metric][model].values())
+
+        plot_file_full_path = args.plot_save_dir + "/roc/" + metric + "_" + \
+            model + "_" + time_stamp + ".png"
+        
+        # plotting:
+        # ROC --> x = FAR, y = GAR
+        plot(
+            x_axis = [far],
+            y_axis = [gar],
+            x_label = "FAR", y_label = "GAR",
+            line_label = ["ROC"],
+            plot_name = f"ROC\nDistance metric: {metric}. Deep Learning model: {model}",
+            show_plot = args.show_plot,
+            plot_file_full_path = plot_file_full_path,
+            x_axis_scale = "linear", y_axis_scale = "linear"
+        )
+
+        plot_file_full_path = args.plot_save_dir + "/det/" + metric + "_" + \
+            model + "_" + time_stamp + ".png"
+        
+        # plotting:
+        # DET (logarithmic scale) --> x = FAR, y = FRR
+        plot(
+            x_axis = [far],
+            y_axis = [frr],
+            x_label = "FAR", y_label = "FRR",
+            line_label = ["DET"],
+            plot_name = f"DET\nDistance metric: {metric}. Deep Learning model: {model}",
+            show_plot = args.show_plot,
+            plot_file_full_path = plot_file_full_path,
+            # TODO DET should take logarithmic scale for both axes
+            # find a way to set them
+            x_axis_scale = "linear", y_axis_scale = "linear"
+        )
--- utils/plotter.py
+++ utils/plotter.py
@@ -3,10 +3,12 @@ import os
 
 
 def plot(
-    x_axis, y_axis,
-    x_label = "x - axis", y_label = "y - label", line_label = [],
-    plot_name = " ", plot = True,
-    plot_file_name_with_path = None,
+    x_axis = None, y_axis = None,
+    x_label = "x - axis", y_label = "y - label", 
+    line_label = [],
+    plot_name = " ", 
+    show_plot = True,
+    plot_file_full_path = None,
     x_axis_scale = "linear", y_axis_scale = "linear",
     ticks_to_use = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 ):
@@ -18,7 +20,10 @@ def plot(
     # giving a title to my graph
     plt.title(plot_name)
 
+    # setting scale of x axis
     plt.xscale(x_axis_scale)
+
+    # setting scale of y axis
     plt.yscale(y_axis_scale)
 
     if (ticks_to_use != None):
@@ -34,96 +39,11 @@ def plot(
 
     plt.legend()
 
-    if (plot_file_name_with_path): plt.savefig(plot_file_name_with_path)
+    if (plot_file_full_path): plt.savefig(plot_file_full_path)
 
     # function to show the plot
-    if (plot):
+    if (show_plot):
         plt.show()
         plt.draw()
 
     plt.clf()
-
-# TODO finish to code this
-# take all needed params from outside!
-def __compute_threshold_FAR_FRR_plot(x_axis_list, y_axis_list):
-    plot(
-        x_axis_list,
-        y_axis_list,
-        [ "thresholds" ], [ "False Rejection Rate", "False Acceptance Rate" ],
-        [ "False Rejection Rate", "False Acceptance Rate" ],
-        "thresholds VS. FRR and FAR", show_plot, plot_name,
-        "linear", "linear"
-    )
-
-# TFF: x = thresholds, y = FRR, FAR
-def compute_threshold_FAR_FRR_plot(distance_metric_name, show_plot, file_name, results):
-
-    folder_path = f"../plots/{distance_metric_name}/{thresholds[0]}_" + \
-        f"{thresholds[-1]}_{int(len(thresholds))}"
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    plot_name = f"{folder_path}/tff_{file_name}.png"
-
-    plot(
-        [ thresholds, thresholds ],
-        [
-            results['false_rejection_rate'][distance_metric_name].values(),
-            results['false_acceptance_rate'][distance_metric_name].values()
-        ],
-        [ "thresholds" ], [ "False Rejection Rate", "False Acceptance Rate" ],
-        [ "False Rejection Rate", "False Acceptance Rate" ],
-        "thresholds VS. FRR and FAR", show_plot, plot_name,
-        "linear", "linear"
-    )
-
-# ROC: x = FAR, y = GAR
-def compute_ROC_plot(distance_metric_name, show_plot, file_name, results):
-
-    folder_path = f"../plots/{distance_metric_name}/{thresholds[0]}_" + \
-        f"{thresholds[-1]}_{int(len(thresholds))}"
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    plot_name = f"{folder_path}/roc_{file_name}.png"
-
-
-    plot(
-        [ results['false_acceptance_rate'][distance_metric_name].values() ],
-        [ results['genuine_acceptance_rate'][distance_metric_name].values() ],
-        [ "False Acceptance Rate" ], [ "Genuine Acceptance Rate" ], ["ROC"],
-        "ROC", show_plot, plot_name,
-        "linear", "linear"
-    )
-
-# DET (logarithmic scale): x = FAR, y = FRR
-def compute_DET_plot(distance_metric_name, show_plot, file_name, results):
-    folder_path = f"../plots/{distance_metric_name}/{thresholds[0]}_" + \
-        f"{thresholds[-1]}_{int(len(thresholds))}"
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    plot_name = f"{folder_path}/det_{file_name}.png"
-
-    plot(
-        [results['false_acceptance_rate'][distance_metric_name].values()],
-        [results['false_rejection_rate'][distance_metric_name].values()],
-        ["False Acceptance Rate"], ["Genuine Acceptance Rate"], ["DET"],
-        "DET", show_plot, plot_name,
-        # TODO
-        # BE WARE! should use "log" "log"
-        # BUT current values are too low so log display does not work.
-        # "log", "log",
-        "linear", "linear"
-    )
-
-def compute_plots(show_plot, file_name):
-
-    for metric in distance_metrics:
-        compute_threshold_FAR_FRR_plot(metric, show_plot, file_name, results)
-
-        compute_ROC_plot(metric, show_plot, file_name, results)
-
-        compute_DET_plot(metric, show_plot, file_name, results)
-
-# compute_plots(bool(int(sys.argv[2])), "TODO add plot name")
