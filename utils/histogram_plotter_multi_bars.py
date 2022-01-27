077eedd2042167b2cecf2e2969d1a6b54f10e1a1
Daniele Solombrino <daniele.solombrino@gmail.com>
2022-01-27 21:54:13 +0100

    improve histogram_plotter_multi

--- utils/histogram_plotter_multi_bars.py
+++ utils/histogram_plotter_multi_bars.py
@@ -51,31 +51,43 @@ tot_dict = {}
 for model in labels:
     tot_dict[model] = dict[model]["correct"] + dict[model]["wrong"]
 
-sample1 = [dict[model]["correct"] / tot_dict[model] * 100 for model in labels]
-sample2 = [dict[model]["wrong"] / tot_dict[model] * 100 for model in labels]
+sample1 = [
+    round(dict[model]["correct"] / tot_dict[model] * 100, 2) for model in labels
+]
+sample2 = [
+    round(dict[model]["wrong"] / tot_dict[model] * 100, 2) for model in labels
+]
 
 x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, sample1, width, label='Error 1')
-rects2 = ax.bar(x + width/2, sample2, width, label='Error 2')
+ax2 = ax.twinx()
+rects1 = ax.bar(
+    x - width/2, sample1, width, label = 'Correct recognition', color = "g"
+)
+rects2 = ax2.bar(
+    x + width/2, sample2, width, label = 'Wrong recognition', color = "r"
+)
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel("Correct & wrong recognition (percentages)")
+ax.set_ylabel("Correct recognition (percentages, higher is better)")
+ax2.set_ylabel("Wrong recognition (percentages, lower is better)")
 ax.set_xlabel("Deep Learning models")
 ax.set_title("Correct & wrong recognition percentages per model")
 ax.set_xticks(x, labels)
-ax.legend()
 
 ax.bar_label(rects1, padding=3)
-ax.bar_label(rects2, padding=3)
+ax2.bar_label(rects2, padding=3)
+
+# to make y axes reach 100
+ax.plot(0, 100)
+ax2.plot(0, 100)
 
 fig.tight_layout()
 
-plt.plot(0, 100)
-plt.show()
+fig.legend(bbox_to_anchor=(0.87, 0.9))
 
+plt.show()
 
 time_stamp = datetime.fromtimestamp(time.time()).strftime('%y_%m_%d_%H-%M-%S')
 plt.savefig("../plots/emotion/" + time_stamp + ".png", dpi = 300)
